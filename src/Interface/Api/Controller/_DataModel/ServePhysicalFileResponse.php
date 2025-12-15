b85d6916620f957d1ff992fc105bf239d15227d3
Kevin Tsiory <tsioryrakotosoa@gmail.com>
2025-12-15 16:32:12 +0300

    feat(MGQ-103): Adding _DataModel of File Controller in Interface.

---
+++ src/Interface/Api/Controller/_DataModel/ServePhysicalFileResponse.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DevFighters\Utils\Interface\Api\Controller\_DataModel;
+
+use DevFighters\Utils\Application\DTO\File\PhysicalFileDTO;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+
+class ServePhysicalFileResponse extends BinaryFileResponse {
+
+    public function __construct(
+        PhysicalFileDTO $file,
+        int $status = 200,
+        array $headers = [],
+        bool $public = true,
+        ?string $contentDisposition = null,
+        bool $autoEtag = false,
+        bool $autoLastModified = true) {
+
+        parent::__construct(
+            $file->getPhysicalPath(),
+            $status,
+            $headers,
+            $public,
+            $contentDisposition,
+            $autoEtag,
+            $autoLastModified);
+
+        $this->setContentDisposition(
+            disposition: ResponseHeaderBag::DISPOSITION_INLINE,
+            filename: $file->getName()
+        );
+    }
+
+}
