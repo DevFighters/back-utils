f7e15f82d86fcb2a27ce59f91dca3ab6c391cb25
Romain FRANCOIS <romain.francois@dev-fighters.fr>
2026-01-20 11:30:36 +0100

    feat: repository doc

--- src/Domain/RepositoryUtils/RepositoryAbstract.php
+++ src/Domain/RepositoryUtils/RepositoryAbstract.php
@@ -6,26 +6,51 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\DBAL\LockMode;
 use Doctrine\ORM\EntityNotFoundException;
 
+/**
+ * @template TEntity of object
+ * @extends ServiceEntityRepository<TEntity>
+ */
 abstract class RepositoryAbstract extends ServiceEntityRepository
 {
-
-    public function find(mixed $id, LockMode|int|null $lockMode = null, int|null $lockVersion = null): ?object
-    {
-        if (is_null($id)) {
+    /**
+     * @return TEntity|null
+     */
+    public function find(
+        mixed $id,
+        LockMode|int|null $lockMode = null,
+        int|null $lockVersion = null
+    ): ?object {
+        if ($id === null) {
             return null;
         }
-        return parent::find($id, $lockMode, $lockVersion);
+
+        /** @var TEntity|null $entity */
+        $entity = parent::find($id, $lockMode, $lockVersion);
+
+        return $entity;
     }
 
-    public function findOne(mixed $id, LockMode|int|null $lockMode = null, int|null $lockVersion = null): object
-    {
-        if (is_null($id)) {
-            throw new EntityNotFoundException("Entity not found : id is null");
+    /**
+     * @return TEntity
+     * @throws EntityNotFoundException
+     */
+    public function findOne(
+        mixed $id,
+        LockMode|int|null $lockMode = null,
+        int|null $lockVersion = null
+    ): object {
+        if ($id === null) {
+            throw new EntityNotFoundException('Entity not found: id is null');
         }
-        $result = $this->find($id, $lockMode, $lockVersion);
-        if (is_null($result)) {
-            throw new EntityNotFoundException("Entity not found : {$this->getEntityName()} with id $id");
+
+        $entity = $this->find($id, $lockMode, $lockVersion);
+
+        if ($entity === null) {
+            throw new EntityNotFoundException(
+                sprintf('Entity not found: %s with id %s', $this->getEntityName(), $id)
+            );
         }
-        return $result;
+
+        return $entity;
     }
 }
