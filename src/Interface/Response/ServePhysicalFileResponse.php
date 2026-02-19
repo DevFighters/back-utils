7a6a0516398a6ed429ff75984c2e13db5b495efd
Romain FRANCOIS <romain.francois@dev-fighters.fr>
2026-02-19 09:36:11 +0100

    ok

--- src/Interface/Response/ServePhysicalFileResponse.php
+++ src/Interface/Response/ServePhysicalFileResponse.php
@@ -9,13 +9,13 @@ use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 class ServePhysicalFileResponse extends BinaryFileResponse
 {
     /**
-     * @param PhysicalFileDTO $file               The file to stream
-     * @param int             $status             The response status code (200 "OK" by default)
-     * @param array           $headers            An array of response headers
-     * @param bool            $public             Files are public by default
-     * @param string|null     $contentDisposition The type of Content-Disposition to set automatically with the filename
-     * @param bool            $autoEtag           Whether the ETag header should be automatically set
-     * @param bool            $autoLastModified   Whether the Last-Modified header should be automatically set
+     * @param PhysicalFileDTO                $file               The file to stream
+     * @param int                            $status             The response status code (200 "OK" by default)
+     * @param array<string, string|string[]> $headers            An array of response headers
+     * @param bool                           $public             Files are public by default
+     * @param string|null                    $contentDisposition The type of Content-Disposition to set automatically with the filename
+     * @param bool                           $autoEtag           Whether the ETag header should be automatically set
+     * @param bool                           $autoLastModified   Whether the Last-Modified header should be automatically set
      */
     public function __construct(
         PhysicalFileDTO $file,
